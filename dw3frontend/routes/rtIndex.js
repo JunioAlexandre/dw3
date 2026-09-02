b4f594ad39e1a1e04fe744cbee5817b590bd36e2
JunioAlexandre <junioalexandre1337@gmail.com>
2026-09-02 10:17:19 -0300

    update aula 02 09

---
+++ dw3frontend/routes/rtIndex.js
@@ -0,0 +1,9 @@
+// Conteúdo do arquivo routes/rtIndex.js
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.redirect('/login');
+});
+
+module.exports = router;
